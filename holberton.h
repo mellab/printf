ab9d5ba252a650293aaddaeb4fab26c78647cc1b
PauloMorillo <pauloan@hotmail.com>
2019-07-29 19:26:29 +0000

    fixed with prototypes and library stdarg

--- holberton.h
+++ holberton.h
@@ -1,6 +1,6 @@
 #ifndef _holberton_h_
 #define _holberton_h_
-
+#include <stdarg.h>
 /**
  * struct formatarg - Struct of elements
  * @forma: data type format
@@ -19,5 +19,7 @@ int _printf(const char *format, ...);
 void printch(va_list valist);
 void prints(va_list valist);
 void printdi(va_list valist);
+int _putchar(char c);
+void _puts(char *str);
 
 #endif /* _holberton_h_ */
