e26111344a00ffd84efa59fcad484aa0c530416d
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-07-29 14:12:38 -0500

    Create the holberton.h file - closes#2

---
+++ holberton.h
@@ -0,0 +1,23 @@
+#ifndef _holberton_h_
+#define _holberton_h_
+
+/**
+ * struct formatarg - Struct of elements
+ * @forma: data type format
+ * @f: pointer to function
+ *
+ */
+
+typedef struct formatarg
+{
+	char *forma;
+	void (*f)(va_list);
+
+} formatico;
+
+int _printf(const char *format, ...);
+void printch(va_list valist);
+void prints(va_list valist);
+void printi(va_list valist);
+
+#endif /* _holberton_h_ */
